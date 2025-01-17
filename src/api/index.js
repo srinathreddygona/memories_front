a0f1a9f9fd09befcc6dd121b0a4aba8f645fe268
srinathreddygona <gsrinathreddy12@gmail.com>
2025-01-17 11:12:35 +0530

    Co-authored-by: Mamidi Ravi Chendraa <Ravichendraa@users.noreply.github.com>

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@
 
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memories-back-postit.onrender.com/posts' });
 // const url='https://memories-back-postit.onrender.com/posts';
 
 
