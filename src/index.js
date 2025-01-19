e560c551a0c5c8776b07ad46bde6e19f6820f413
srinathreddygona <150712018+srinathreddygona@users.noreply.github.com>
2025-01-19 23:16:39 +0530

    hardcodedid

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const store=legacy_createStore(reducers,compose(applyMiddleware(thunk)));
 const CLIENT_ID=process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 ReactDOM.render(
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <GoogleOAuthProvider clientId="358467555656-r6j6e49ra6bcpmosr4h34fehfauf6hg9.apps.googleusercontent.com">
 <Provider store={store}>
 <App/>
 </Provider>
