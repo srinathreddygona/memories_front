37296faedd83a31f7de8eb71cfef5699fb3b7e57
srinathreddygona <150712018+srinathreddygona@users.noreply.github.com>
2025-01-19 20:16:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const store=legacy_createStore(reducers,compose(applyMiddleware(thunk)));
 const CLIENT_ID=process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 ReactDOM.render(
-    <GoogleOAuthProvider clientId={`${CLIENT_ID}`}
+    <GoogleOAuthProvider clientId={CLIENT_ID}
 }>
 <Provider store={store}>
 <App/>
